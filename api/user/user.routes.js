9168ab5b1527afc76f43a699f76363dcfa8a7908
Firmansyah Yanuar <fyfirman@gmail.com>
2020-05-12 01:45:45 +0700

    change register routes

--- api/user/user.routes.js
+++ api/user/user.routes.js
@@ -6,9 +6,9 @@ const routes = "/user";
 module.exports = (router) => {
   router.get(routes + "/", User.getUsers);
   router.get(routes + "/:id", User.getUser);
-  router.post(routes + "/", validate('createUser'), User.createUser);
   router.put(routes + "/:id", User.updateUser);
   router.delete(routes + "/:id", User.removeUser);
-
+  
+  router.post("/register", validate('createUser'), User.createUser);
   router.post("/login", User.authenticate)
 };
