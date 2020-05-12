0facc7beb7c78a6ec1b3c85d828f6dae5c22a540
Firmansyah Yanuar <fyfirman@gmail.com>
2020-05-13 03:03:00 +0700

    edit stuff schema

--- api/stuff/stuff.model.js
+++ api/stuff/stuff.model.js
@@ -2,21 +2,30 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Stuff = new Schema({
-    _id: { type: String },
-    name: { type: String },
-    description: { type: String },
-    picture: { type: String },
-    status: {
-        type: String,
-        enum: ['New', 'Former']
-    },
-    amount: { type: String },
-    condition: { type: String },
-    address: { type: String },
-    is_cod: { type: Boolean },
-    postal_fee: { type: String },
-    owner_id: { type: Schema.Types.ObjectId, ref: 'User' },
-    timestamps: true
+  name: {
+    type: String,
+    required: true
+  },
+  description: { type: String },
+  picture: { type: String },
+  status: {
+    type: Boolean,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  condition: {
+    type: String,
+    enum: ['New', 'Former'],
+    required: true
+  },
+  address: { type: String },
+  is_cod: { type: Boolean },
+  postal_fee: { type: String },
+  owner_id: { type: Schema.Types.ObjectId, ref: 'User' },
+  timestamps: true
 });
 
 module.exports = mongoose.model('Stuff', Stuff);
