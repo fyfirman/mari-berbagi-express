98ee6552ed97f45bf663168cff92ee6fad1dc8ab
Firmansyah Yanuar <fyfirman@gmail.com>
2020-05-19 16:30:15 +0700

    fix request model

--- api/request/request.model.js
+++ api/request/request.model.js
@@ -2,20 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Request = new Schema({
-    _id: { type: String },
-    name: { type: String },
-    reason: { type: String },
-    amount: { type: String },
-    address: { type: String },
-    is_cod: { type: Boolean },
-    postal_fee: { type: String },
-    status: {
-        type: String,
-        enum: ['Paid', 'Not yet paid', 'Waiting', 'Rejected']
-    },
-    requestor_id: { type: Schema.Types.ObjectId, ref: 'User' },
-    stuff_id: { type: Schema.Types.ObjectId, ref: 'Stuff' },
-    timestamps: true
+  _id: { type: String },
+  name: { type: String },
+  reason: { type: String },
+  amount: { type: String },
+  address: { type: String },
+  is_cod: { type: Boolean },
+  postal_fee: { type: Boolean },
+  status: {
+    type: String,
+    enum: ['Paid', 'Not yet paid', 'Waiting', 'Rejected']
+  },
+  requestor_id: { type: Schema.Types.ObjectId, ref: 'User' },
+  stuff_id: { type: Schema.Types.ObjectId, ref: 'Stuff' },
+  timestamps: true
 });
 
 module.exports = mongoose.model('Request', Request);
