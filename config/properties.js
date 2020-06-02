9075e1ff10758ce1471eead74e85c3a8c8107b95
Firmansyah Yanuar <fyfirman@gmail.com>
2020-06-02 08:43:55 +0700

    change port to 80

--- config/properties.js
+++ config/properties.js
@@ -1,4 +1,4 @@
 module.exports = {
-  PORT: 4000,
+  PORT: 80,
   DB: "mongodb://admin:mariberbagi@mari-berbagi-shard-00-00-yto6h.mongodb.net:27017,mari-berbagi-shard-00-01-yto6h.mongodb.net:27017,mari-berbagi-shard-00-02-yto6h.mongodb.net:27017/test?ssl=true&replicaSet=mari-berbagi-shard-0&authSource=admin&retryWrites=true&w=majority",
 };
