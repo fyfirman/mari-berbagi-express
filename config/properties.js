28e1e2ddb46d720a5c2fb4a1acce033437d44193
Firmansyah Yanuar <fyfirman@gmail.com>
2020-06-02 08:42:32 +0700

    downgrade mongo uri version

--- config/properties.js
+++ config/properties.js
@@ -1,4 +1,4 @@
 module.exports = {
   PORT: 4000,
-  DB: "mongodb+srv://admin:mariberbagi@mari-berbagi-yto6h.mongodb.net/test?retryWrites=true&w=majority",
+  DB: "mongodb://admin:mariberbagi@mari-berbagi-shard-00-00-yto6h.mongodb.net:27017,mari-berbagi-shard-00-01-yto6h.mongodb.net:27017,mari-berbagi-shard-00-02-yto6h.mongodb.net:27017/test?ssl=true&replicaSet=mari-berbagi-shard-0&authSource=admin&retryWrites=true&w=majority",
 };
